235fc7e555897bab18fca69d48f094fa1eee17b0
andika98 <121611042@mhs.uqgresik.ac.id>
2023-07-05 12:34:33 +0700

    url api-0.1.2

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ fetch('https://coba-api-pink.vercel.app/api/data')
     console.log(data);
     // Contoh: Menampilkan data di elemen dengan ID "data-container"
     const dataContainer = document.getElementById('data-container');
+
+    dataContainer.innerHTML = `
+    <p>Nama : ${data[0].name}</p>
+    <p>Nama : ${data[0].age}</p>
+    <p>Nama : ${data[0].timestamp}</p>
+    `;
     dataContainer.innerText = JSON.stringify(data);
   })
   .catch(error => console.log(error));
